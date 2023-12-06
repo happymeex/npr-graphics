0a07174abfef11c0e4f710938db3b6a3bce8fe1e
happymeex <32947316+happymeex@users.noreply.github.com>
2023-12-06 12:03:32 -0500

    move include functional to Image.hpp, reformatting

--- src/Image.hpp
+++ src/Image.hpp
@@ -1,6 +1,7 @@
 #ifndef IMAGE
 #define IMAGE
 
+#include <functional>
 #include <glm/glm.hpp>
 #include <string>
 #include <vector>
@@ -9,55 +10,54 @@ const glm::mat3 GX(1, 2, 1, 0, 0, 0, -1, -2, -1);
 const glm::mat3 GY(1, 0, -1, 2, 0, -2, 1, 0, -1);
 const glm::mat3 GAUSS(1, 2, 1, 2, 4, 2, 1, 2, 1);
 
-class Image
-{
-public:
-  Image(int width, int height) : width_(width), height_(height)
-  {
-    data_ = std::vector<glm::vec4>(width * height);
-  }
+class Image {
+  public:
+    Image(int width, int height) : width_(width), height_(height) {
+        data_ = std::vector<glm::vec4>(width * height);
+    }
 
-  int GetWidth() const { return width_; }
+    int GetWidth() const { return width_; }
 
-  int GetHeight() const { return height_; }
+    int GetHeight() const { return height_; }
 
-  const glm::vec4 &GetPixel(int x, int y) const;
-  /**
-   * Sets the color of a pixel at the specified coordinates in the image.
-   *
-   * @param x The x-coordinate of the pixel.
-   * @param y The y-coordinate of the pixel.
-   * @param color The color to set the pixel to.
-   *
-   * @throws const char* An exception is thrown if the coordinates are out of
-   * range.
-   */
-  void SetPixel(int x, int y, glm::vec4 color);
+    const glm::vec4 &GetPixel(int x, int y) const;
+    /**
+     * Sets the color of a pixel at the specified coordinates in the image.
+     *
+     * @param x The x-coordinate of the pixel.
+     * @param y The y-coordinate of the pixel.
+     * @param color The color to set the pixel to.
+     *
+     * @throws const char* An exception is thrown if the coordinates are out of
+     * range.
+     */
+    void SetPixel(int x, int y, glm::vec4 color);
 
-  /**
-   * Saves the image as a PNG file in the `out/` directory.
-   * Prints an error message if the file cannot be saved.
-   *
-   * @param file_name The name of the file to save the image to.
-   */
-  void SavePNG(const std::string &file_name) const;
+    /**
+     * Saves the image as a PNG file in the `out/` directory.
+     * Prints an error message if the file cannot be saved.
+     *
+     * @param file_name The name of the file to save the image to.
+     */
+    void SavePNG(const std::string &file_name) const;
 
-  Image ApplyLayer(const Image &image2,
-                   const std::function<glm::vec3(glm::vec3, glm::vec3)> &function) const;
+    Image ApplyLayer(
+        const Image &image2,
+        const std::function<glm::vec3(glm::vec3, glm::vec3)> &function) const;
 
-  Image ApplyFilter(const glm::mat3 &filter,
-                    bool on_transparency = false) const;
+    Image ApplyFilter(const glm::mat3 &filter,
+                      bool on_transparency = false) const;
 
-  Image GetEdges() const;
+    Image GetEdges() const;
 
-private:
-  int width_;
-  int height_;
-  /**
-   * Row-major order pixel RGBA data for the image.
-   * All values are in the range [0, 1].
-   */
-  std::vector<glm::vec4> data_;
+  private:
+    int width_;
+    int height_;
+    /**
+     * Row-major order pixel RGBA data for the image.
+     * All values are in the range [0, 1].
+     */
+    std::vector<glm::vec4> data_;
 };
 
 #endif
--- src/Tracer.cpp
+++ src/Tracer.cpp
@@ -1,15 +1,11 @@
 #include "Tracer.hpp"
-#include <functional>
 #include <glm/gtx/string_cast.hpp>
 #include <iostream>
 
-Image Tracer::RenderOnce(RenderStyle style)
-{
+Image Tracer::RenderOnce(RenderStyle style) {
     Image image(width_, height_);
-    for (int y = 0; y < height_; y++)
-    {
-        for (int x = 0; x < width_; x++)
-        {
+    for (int y = 0; y < height_; y++) {
+        for (int x = 0; x < width_; x++) {
             float mapped_x = 2.0f * (float)x / (width_ - 1) - 1.0f;
             float mapped_y = 2.0f * (float)y / (height_ - 1) - 1.0f;
             Ray ray = camera_.GetRay(mapped_x, -mapped_y);
@@ -25,12 +21,10 @@ Image Tracer::RenderOnce(RenderStyle style)
 }
 
 void Tracer::Render(const Scene &scene, const std::string &out_file_name,
-                    RenderStyle style)
-{
+                    RenderStyle style) {
     scene_ = &scene;
 
-    if (style == RenderStyle::Cel)
-    {
+    if (style == RenderStyle::Cel) {
         // Get the beta image to generate the edges
         const auto beta_image = RenderOnce(RenderStyle::Beta);
         const auto beta_edges = beta_image.GetEdges();
@@ -39,61 +33,52 @@ void Tracer::Render(const Scene &scene, const std::string &out_file_name,
         const auto cel_image = RenderOnce(RenderStyle::Cel);
 
         // Multiply to get edged image
-        const auto multiply_inverse = [](glm::vec3 a, glm::vec3 b)
-        {
+        const auto multiply_inverse = [](glm::vec3 a, glm::vec3 b) {
             return a * (glm::vec3(1.0, 1.0, 1.0) - 0.5f * b);
         };
-        const auto edged_image = cel_image.ApplyLayer(beta_edges, multiply_inverse);
+        const auto edged_image =
+            cel_image.ApplyLayer(beta_edges, multiply_inverse);
 
         edged_image.SavePNG(out_file_name);
-    }
-    else if (style == RenderStyle::Real)
-    {
+    } else if (style == RenderStyle::Real) {
         const auto result = RenderOnce(RenderStyle::Real);
         result.SavePNG(out_file_name);
     }
 }
 
 glm::vec3 Tracer::TraceRay(const Ray &ray, int bounces, HitRecord &hit_record,
-                           RenderStyle style)
-{
+                           RenderStyle style) {
     glm::vec3 diffuse_color(0.0f, 0.0f, 0.0f);
     glm::vec3 specular_color(0.0f, 0.0f, 0.0f);
     float shininess = 0.0f;
     bool any_hit = false;
-    for (auto &obj : scene_->children)
-    {
+    for (auto &obj : scene_->children) {
         bool hit = obj->Intersects(ray, 0.0f, hit_record);
         any_hit |= hit;
-        if (hit)
-        {
+        if (hit) {
             auto material = obj->GetMaterial();
             diffuse_color = material.diffuse;
             specular_color = material.specular;
             shininess = material.shininess;
         }
     }
-    if (!any_hit)
-    {
+    if (!any_hit) {
         return background_color_;
     }
 
-    if (style == RenderStyle::Beta)
-    {
+    if (style == RenderStyle::Beta) {
         return diffuse_color;
     }
 
     glm::vec3 color(0.f);
     glm::vec3 hit_pos = ray.origin + ray.direction * hit_record.time;
-    for (const auto &light : scene_->lights)
-    {
+    for (const auto &light : scene_->lights) {
         glm::vec3 dir_to_light;
         glm::vec3 light_intensity;
         float dist_to_light;
         light->GetIllumination(hit_pos, dir_to_light, light_intensity,
                                dist_to_light);
-        if (light->GetType() == LightType::AMBIENT)
-        {
+        if (light->GetType() == LightType::AMBIENT) {
             color += light_intensity * diffuse_color;
             continue;
         }
@@ -102,14 +87,10 @@ glm::vec3 Tracer::TraceRay(const Ray &ray, int bounces, HitRecord &hit_record,
 
         float diffuse_scalar =
             glm::max(0.0f, glm::dot(hit_record.normal, dir_to_light));
-        if (style == RenderStyle::Cel)
-        {
-            if (diffuse_scalar > 0.3)
-            {
+        if (style == RenderStyle::Cel) {
+            if (diffuse_scalar > 0.3) {
                 diffuse_scalar = 0.8;
-            }
-            else
-            {
+            } else {
                 diffuse_scalar = 0.1;
             }
         }
@@ -120,14 +101,10 @@ glm::vec3 Tracer::TraceRay(const Ray &ray, int bounces, HitRecord &hit_record,
         glm::vec3 reflection = glm::reflect(-dir_to_light, hit_record.normal);
         float specular_scalar = glm::pow(
             glm::max(0.0f, glm::dot(reflection, view_direction)), shininess);
-        if (style == RenderStyle::Cel)
-        {
-            if (specular_scalar > 0.3)
-            {
+        if (style == RenderStyle::Cel) {
+            if (specular_scalar > 0.3) {
                 specular_scalar = 0.8;
-            }
-            else
-            {
+            } else {
                 specular_scalar = 0.1;
             }
         }
