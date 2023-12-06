9d6b6220b7242fb396d989910fff1c5ba59bc41b
Daniel Hu <danielhu@mit.edu>
2023-12-06 11:42:34 -0500

    typedef uint for mac compatibility

--- src/BVH.hpp
+++ src/BVH.hpp
@@ -6,51 +6,54 @@
 #include "Shapes.hpp"
 #include <vector>
 
-struct BVHNode {
-    glm::vec3 min, max;
-    uint left_idx, num_triangles;
+typedef unsigned int uint;
+struct BVHNode
+{
+  glm::vec3 min, max;
+  uint left_idx, num_triangles;
 };
-class BVHObject {
-  public:
-    BVHObject(std::vector<Triangle> &triangles)
-        : nodes_created(0), triangles_(triangles){};
-    void ConstructBVH();
-    bool Intersects(const Ray &ray, float t_min, HitRecord &record,
-                    uint node_id = 0);
+class BVHObject
+{
+public:
+  BVHObject(std::vector<Triangle> &triangles)
+      : nodes_created(0), triangles_(triangles){};
+  void ConstructBVH();
+  bool Intersects(const Ray &ray, float t_min, HitRecord &record,
+                  uint node_id = 0);
 
-  private:
-    uint nodes_created;
-    std::vector<BVHNode> nodes_;
-    std::vector<Triangle> &triangles_;
-    std::vector<glm::vec3> centroids_;
-    /**
-     * Tracks whether a node in the BVH is a leaf or not.
-     */
-    std::vector<bool> is_leaf_;
-    /**
-     * Reordering of the triangles: triangle_indices_[i] returns an index
-     * of the i-th triangle in the BVH, namely an index for a triangle in
-     * `triangles_`.
-     *
-     * The fields `left_idx` and `num_triangles` of the BVH nodes are
-     * used to index into `triangle_indices_`.
-     */
-    std::vector<uint> triangle_indices_;
-    /**
-     * Sets the bounding box of a node in the BVH based on the triangles
-     * currently assigned to the node.
-     */
-    void SetNodeBoundingBox(uint node_id);
-    /**
-     * Recursively splits a node in the BVH.
-     */
-    void SplitNode(uint node_id);
-    /**
-     * Checks if a ray intersects the bounding box of a node in the BVH.
-     * Does not mutate the hit record.
-     */
-    bool IntersectsBoundingBox(const Ray &ray, float t_min,
-                               const HitRecord &record, const BVHNode &node);
+private:
+  uint nodes_created;
+  std::vector<BVHNode> nodes_;
+  std::vector<Triangle> &triangles_;
+  std::vector<glm::vec3> centroids_;
+  /**
+   * Tracks whether a node in the BVH is a leaf or not.
+   */
+  std::vector<bool> is_leaf_;
+  /**
+   * Reordering of the triangles: triangle_indices_[i] returns an index
+   * of the i-th triangle in the BVH, namely an index for a triangle in
+   * `triangles_`.
+   *
+   * The fields `left_idx` and `num_triangles` of the BVH nodes are
+   * used to index into `triangle_indices_`.
+   */
+  std::vector<uint> triangle_indices_;
+  /**
+   * Sets the bounding box of a node in the BVH based on the triangles
+   * currently assigned to the node.
+   */
+  void SetNodeBoundingBox(uint node_id);
+  /**
+   * Recursively splits a node in the BVH.
+   */
+  void SplitNode(uint node_id);
+  /**
+   * Checks if a ray intersects the bounding box of a node in the BVH.
+   * Does not mutate the hit record.
+   */
+  bool IntersectsBoundingBox(const Ray &ray, float t_min,
+                             const HitRecord &record, const BVHNode &node);
 };
 
 #endif
